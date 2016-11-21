184959fed047bd09d9a0cd230f8ea592b3c361cf
Michael Wiktorek <wiktorekmichael@gmail.com>
2016-11-21 15:05:43 -0800

    added css

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Markov text generator in ES6 (I guess)
 
 Takes some input text, builds a big markov chain, then spits out some more text that should sound like the original but mean nothing.
 
-I originally wrote this in coffeescript and compiled it with gulp. It required a huge number of dependencies and a build script and you needed to have node.js installed etc etc. Recently I realized that much of that was a waste of time, and for a small project like this using plain Javascript is much better. Therefore, I translated it directly into ES6.
+I originally wrote this in coffeescript and compiled it with gulp. It required a huge number of dependencies and a build script and you needed to have node.js installed etc etc. Recently I realized that much of that was a waste of time, and for a small project like this using plain Javascript is much better. Therefore, I translated it directly into JS.
--- markov_text.js
+++ markov_text.js
@@ -1,127 +1,138 @@
 function exists(thing) {
-    return typeof(thing) !== "undefined" && thing !== null;
+  return typeof(thing) !== "undefined" && thing !== null;
 }
 
 function alert_no_chain() {
-    alert("You need to build a markov chain first!");
+  alert("You need to build a markov chain first!");
 }
 
 class MarkovChain {
-    constructor(text, k) {
-	this.dict = {};
-	this.k = k;
-	this.text = text;
-	for (let i = 0; i < text.length; i++) {
-	    var key = '';
-	    var value = '';
-	    if (i < text.length - this.k) {
-		key = text.substring(i, i+this.k);
-		value = text.substring(i+1, i+this.k+1);
-	    } else {
-		var diff = (i+this.k) - text.length;
-		key = text.substring(i, i+this.k) + text.substring(0, diff);
-		value = text.substring(i+1, i+this.k+1) + text.substring(0, diff+1);
-	    }
-	    this.add_transition(key, value);
-	}
+  constructor(text, k) {
+    this.dict = {};
+    this.k = k;
+    this.text = text;
+    for (let i = 0; i < text.length; i++) {
+      var key = '';
+      var value = '';
+      if (i < text.length - this.k) {
+        key = text.substring(i, i+this.k);
+        value = text.substring(i+1, i+this.k+1);
+      } else {
+        var diff = (i+this.k) - text.length;
+        key = text.substring(i, i+this.k) + text.substring(0, diff);
+        value = text.substring(i+1, i+this.k+1) + text.substring(0, diff+1);
+      }
+      this.add_transition(key, value);
     }
+  }
 
-    add_transition(v, w) {
-	if (!exists(this.dict[v])) {
-	    this.dict[v] = [];
-	}
-	this.dict[v].push(w);
-	if (!exists(this.dict[w])) {
-	    this.dict[w] = [];
-	}
+  add_transition(v, w) {
+    if (!exists(this.dict[v])) {
+      this.dict[v] = [];
     }
+    this.dict[v].push(w);
+    if (!exists(this.dict[w])) {
+      this.dict[w] = [];
+    }
+  }
 
-    next(v) {
-	var len = this.dict[v].length;
-	var rand = Math.floor(Math.random() * len);
-	return this.dict[v][rand];
-    }
+  next(v) {
+    var len = this.dict[v].length;
+    var rand = Math.floor(Math.random() * len);
+    return this.dict[v][rand];
+  }
 
-    print() {
-	console.log("keys: ");
-	for (let i of this.dict) {
-	    console.log(i);
-	}
+  print() {
+    console.log("keys: ");
+    for (var i in this.dict) {
+      console.log(i);
     }
+  }
 
-    print_neighbors(v) {
-	var len = this.dict[v].length;
-	console.log(len);
-	for (let i = 0; i < len-1; i++) {
-	    console.log(this.dict[v][i]);
-	}
+  print_neighbors(v) {
+    var len = this.dict[v].length;
+    console.log(len);
+    for (let i = 0; i < len-1; i++) {
+      console.log(this.dict[v][i]);
     }
+  }
 
-    last_char(v) {
-	return v.slice(-1);
-    }
+  last_char(v) {
+    return v.slice(-1);
+  }
 
-    generate_text(length) {
-	var m = parseInt(length, 10);
-	var state = this.text.slice(0, this.k);
-	var output = "" + state;
-	for (let i = 0; i < m-this.k; i++) {
-	    state = this.next(state);
-	    if (state == -1 || !exists(state)) {
-		return output;
-	    }
-	    output = output + this.last_char(state);
-	}
-	return output;
+  generate_text(length) {
+    var m = parseInt(length, 10);
+    var state = this.text.slice(0, this.k);
+    var output = "" + state;
+    for (let i = 0; i < m-this.k; i++) {
+      state = this.next(state);
+      if (state == -1 || !exists(state)) {
+        return output;
+      }
+      output = output + this.last_char(state);
     }
+    return output;
+  }
 }
 
 class Manager {
-    constructor() {
-	console.log("made manager");
-    }
+  constructor() {
+    console.log("made manager");
+  }
 
-    build_chain(text, k) {
-	this.chain = new MarkovChain(text, parseInt(k, 10));
-    }
+  build_chain(text, k) {
+    this.chain = new MarkovChain(text, parseInt(k, 10));
+  }
 
-    generate_text(M) {
-	var m = parseInt(M, 10);
-	if (!exists(this.chain)) {
-	    alert_no_chain();
-	    return "";
-	} else {
-	    return this.chain.generate_text(m);
-	}  
-    }
+  generate_text(M) {
+    var m = parseInt(M, 10);
+    if (!exists(this.chain)) {
+      alert_no_chain();
+      return "";
+    } else {
+      return this.chain.generate_text(m);
+    }  
+  }
 
-    print_keys() {
-	if (exists(this.chain)) {
-	    this.chain.print()
-	}else {
-	    alert_no_chain();
-	}
+  print_keys() {
+    if (exists(this.chain)) {
+      this.chain.print()
+    }else {
+      alert_no_chain();
     }
+  }
 }
 
 window.onload = function() {
-    var manager = new Manager();
-    
-    var build_chain = function() {
-	var k_val = document.querySelector("#kval").value;
-	var text = document.querySelector("#text").value;
-	manager.build_chain(text, k_val);
-    };
+  var manager = new Manager();
 
-    var build_text = function() {
-	var length = document.querySelector("#length").value;
-	var output = manager.generate_text(length);
-	var div = document.querySelector("#output");
-	div.innerHTML = output;
-    };
+  var build_chain = function() {
+    var k_val = document.querySelector("#kval").value;
+    var text = document.querySelector("#text").value;
+    manager.build_chain(text, k_val);
+    document.querySelector("#built").style.display = "block";
+  };
 
-    document.querySelector("#start").onclick = build_chain;
-    document.querySelector("#print").onclick = build_text;
+  var build_text = function() {
+    var length = document.querySelector("#length").value;
+    var output = manager.generate_text(length);
+    var div = document.querySelector("#output");
+    div.innerHTML = output;
+  };
+
+  var log_keys = function() {
+    manager.print_keys();
+  }
+
+  var hide_built = function() {
+    document.querySelector("#built").style.display = "none";
+  }
+
+  document.querySelector("#start").onclick = build_chain;
+  document.querySelector("#print").onclick = build_text;
+  document.querySelector("#keys").onclick = log_keys;
+  document.querySelector("#text").oninput = hide_built;
 };
 
 
